6126490c4217321fb31fe12ede14ca7545b36ff9
Raf <raf@cs.berkeley.edu>
2018-05-31 10:13:41 -0600

    added comments to std example and made it standalone

--- stdmap_rerun_2p0_soficproc.py
+++ stdmap_rerun_2p0_soficproc.py
@@ -7,32 +7,38 @@ import utils
 import networkx as nx
 from sofic_processor_standalone import SoficProcessor
 
+### Load the data file, which has the labeled Conley index rep
+dat = utils.load_matlab_matrix( 'std-eps2-p2-plus34-run-dat.mat', matname='dat')
 
-dat = utils.load_matlab_matrix( '/home/raf/projects/std/std-eps2-p2-runs/std-eps2-p2-plus34-run-dat.mat', matname='dat')
-
-# the [0,0][0,1] is to get the second entry of the cell, i.e. dat.M_inv{2}
+### the [0,0][0,1] retrieves the second cell entry, i.e. dat.M_inv{2}
 M = dat['M_inv'][0,0][0,1]
 G = utils.cell2dict(dat['G_inv'][0,0][0,1][0])
-sof = SoficProcessor(M,G,debug=True)
+
+### Process the labeled Conley index information
+sof = SoficProcessor(M,G,debug=True)  # Set debug to False to silence
 sof.process()
 print sof
 print 'entropy:', sof.entropy()
+### Output:
 # SoficProcessor on 41 symbols, with 68 states and 127 transitions
 # entropy: 0.571595590383
-# up from STD: Theorem 4.4. The topological entropy for the standard map for eps = 2 is bounded below by 0.54518888942276.
+### Note: higher from previous work on the standard map: "Theorem 4.4. The topological entropy for the standard map for eps = 2 is bounded below by 0.54518888942276."
 
+### Take the periodic closure and minimize
 sof.take_periodic_closure(debug=True)
 print "Minimizing..."
 sof.minimize()
 print sof
 comps = list(nx.strongly_connected_components(sof.minimized_mgraph))
 print "Connected components of sizes:", map(len,comps)
-# only keep component 0
+### Only keep one strongly connected component, in this case component 0
 for c in comps[1:]:
     sof.minimized_mgraph.remove_nodes_from(c)
 print sof
 
+### The vertex shift from the original paper
 SM = dat['SM'][0,0][0,1]
 print "Original verified vertex shift had", SM.nnz, "edges"
 
-print sof.sofic_shift_to_latex_graph(layout=nx.graphviz_layout)
+### Uncomment to print the TikZ code for the graph
+# print sof.sofic_shift_to_latex_graph(layout=nx.graphviz_layout)
